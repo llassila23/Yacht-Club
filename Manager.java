b153119885dcd2363e32768896fedf12181b4e1a
Lucas Lassila <129896722+llassila23@users.noreply.github.com>
2023-05-27 17:19:32 -0700

    Add files via upload
    Changes:
    1. print() now can print the Search Tree in order, It is a recursive method that starts at the root, then traverses the left side, then the right side of a given node. This is done by sub method printInO(). print() and printInO() also call sub method printToString() which simply outputs the data at of given node to console.
    
    printToString() will be very useful down the line when editing and deleting as well as those methods won't need their own print function to handle the large amount of  data in each Tree Node. 
    
    2. The genKey() method now contains a random number that is added to the key value. When testing it was common for there to be two keys of very similar or equal value. This obviously causes issues if they are equal. Now and number from 0-49 is added to each key, seperating them and reducing the chance of this occuring.
    
    3. There is now a second if else condition in addNode()'s main body. This will set the child nodes of the root if there are none already before going into the traversal method. This fixed a bug where the root would appear to have been replaced by the new node. It also makes the program faster because if there is no need to traverse it will not have to.

--- Manager.java
+++ Manager.java
@@ -5,12 +5,15 @@
    Purpose: This is the Manager class. It will define the primary methods using the TreeNode Object
  */
 
-
+ 
+//import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 
+
 public class Manager {
-    
+// note private methods are intended to distinguish them as "Sub methods"
 // the following variables are set everytime the user creates a new entry.
     private int key = 0; // search key integer
 
@@ -22,9 +25,11 @@ public class Manager {
     private String state = null;
     private String zip = null;
 
+    //ArrayList<Integer>  keySet = new ArrayList<Integer>(); // will contain all keys
+
     Scanner input = new Scanner(System.in);
 
-    private TreeNode root = new TreeNode(key, lName, fName, phoneNum, address, city, state, zip, null, null);  // create local root node with null values
+    TreeNode root = new TreeNode(key, lName, fName, phoneNum, address, city, state, zip, null, null);  // create local root node with null values
 
 
 // method definitions begin here
@@ -32,7 +37,7 @@ public class Manager {
         // initilal values for root node
         fName = "Yacht";
         lName = "Club";
-        phoneNum = "406123CLUB";
+        phoneNum = "123-456-0901";
 
         key = genKey(); // generate key
         
@@ -57,6 +62,10 @@ public class Manager {
     //precondtion: fName, lName and phoneNum have been set -> getData must always be run prior to genKey to ensure a unique key
     public int genKey(){
 
+        Random rand = new Random();
+        int randNum = rand.nextInt(50); // random number from 0 to 15 to help seperate key values
+
+
         // char val for initials
         char fN = fName.charAt(0);
         char lN = lName.charAt(0);
@@ -79,26 +88,43 @@ public class Manager {
             num10 = phoneNum.charAt(6);
         }
 
-        key = (fN-0) + (lN-0) + (num8 -0) + (num9 - 0) + (num10 - 0); // makes a key for that given entry
+        key = (fN-0) + (lN-0) + (num8 -0) + (num9 - 0) + (num10 - 0) + randNum; // makes a key for that given entry
         System.out.println(key);
+
+        //keySet.add(key); // add key to keySet
         return key;
         // reset key to zero after it is used to make a new node
 
     } // end genKey
 
     // precondition: key = 0 and TreeNode root exists
+    // post condition: node has been added
     public void addNode(){
         getData(); // take user input
         key = genKey(); // generate key given user data
 
+        // does not work. current is set to null everytime
+
+
         TreeNode current = root; // start at top
 
+        
+
         if(key < root.SEARCH_KEY){ // if less than root go to left
+
+            if(root.left == null){ // if first node, no need to search, just add to root.
+                root.left = new TreeNode(key, lName, fName, phoneNum, address, city, state, zip, null, null);
+            }else{
             current = root.left; 
             traverseAndAdd(current);
+            }
         }else if(key > root.SEARCH_KEY){ // greater than root go to right
+            if(root.right == null){
+                root.right = new TreeNode(key, lName, fName, phoneNum, address, city, state, zip, null, null);
+            }else{
             current = root.right; 
             traverseAndAdd(current);
+            }
         } 
         key = 0; // reset key to be zero
     }
@@ -124,12 +150,13 @@ public class Manager {
     }
 
     // precondition: key of new node has been compared to root key and current has been set to the head of the appropriate subtree. 
+    //              first nodes after the root have been set already
     // postcondition: new node has been added
     private void traverseAndAdd(TreeNode current) { // called by addNode and traverses through the subtrees and adds a new node 
 
-        if (current == null){ // case that there is only the root node
-            current = new TreeNode(key, lName, fName, phoneNum, address, city, state, zip, null, null);
-        } // if done, while loop and if else will not run b.c current.L&R are null & key == current.Search_Key
+       // if (current == null){ // case that there is only the root node
+        //    current = new TreeNode(key, fName, lName, phoneNum, address, city, state, zip, null, null);
+        //} // if done, while loop and if else will not run b.c current.L&R are null & key == current.Search_Key
 
         // traverse until null space is found
         while(current.left!= null || current.right != null){
@@ -142,13 +169,66 @@ public class Manager {
 
         // add new nodes to null "roots" of current
         if(key < current.SEARCH_KEY){ // new node to left of current
-            current.left = new TreeNode(key, lName, fName, phoneNum, address, city, state, zip, null, null ); // new node with null "roots"
+            current.left = new TreeNode(key, fName, lName, phoneNum, address, city, state, zip, null, null ); // new node with null "roots"
         } else if ( key > current.SEARCH_KEY){ // new node to right of current
-            current.right = new TreeNode(key, lName, fName, phoneNum, address, city, state, zip, null, null);
+            current.right = new TreeNode(key, fName, lName, phoneNum, address, city, state, zip, null, null);
         } // end add new node if else
     }// end traverseAndAdd
 
-    public void print(){ // method to print tree
+    public void print(){ // method to print tree in different ways
+        TreeNode current = root;
+
+
+        //if(want print in order){}
+        // recursive in order traversal
+        printInO(current);
+
+        
+
+
+
+        
+
+
+        // every key created goes into an array list
+        // can sort this array list for the given traversal types
+        // search() will find the node at that key
+        // printNode(said node with said key)
+
+
+    }
+
+    
+    private void printInO(TreeNode current){
+
+        try {
+
+            printInO(current.left);
+            printNode(current);
+            printInO(current.right);
+
+        } catch (Exception NullPointerException) {
+            return;
+        }
+
+
+        
+    }
+
+
+
+    private void printNode(TreeNode current){
+        System.out.println();
+
+        System.out.print(current.SEARCH_KEY +"" + " ");
+        System.out.print(current.FIRST_NAME + " ");
+        System.out.print(current.LAST_NAME + " ");
+        System.out.print(current.PHONE_NUM + " ");
+        System.out.print(current.ADDRESS + " ");
+        System.out.print(current.CITY + " ");
+        System.out.print(current.STATE + " ");
+        System.out.println(current.ZIP );
+        
 
     }
 
